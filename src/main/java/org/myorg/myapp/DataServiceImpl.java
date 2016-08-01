ad80db86106bb7eab560a0193e66cb44fa08538b
Sebastian Laskawiec <slaskawi@redhat.com>
2016-08-01 13:51:14 +0200

    9.0.0-SNAPSHOT

--- src/main/java/org/myorg/myapp/DataServiceImpl.java
+++ src/main/java/org/myorg/myapp/DataServiceImpl.java
@@ -1,11 +1,11 @@
 package org.myorg.myapp;
 
-import org.infinispan.cdi.ConfigureCache;
-import org.infinispan.configuration.cache.Configuration;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+
+import org.infinispan.cdi.embedded.ConfigureCache;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
 
 @ApplicationScoped
 public class DataServiceImpl {
